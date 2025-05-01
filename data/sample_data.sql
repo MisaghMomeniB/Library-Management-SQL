efcc2f8a14ec8a29c512c7891970cf906dad4d49
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-05-01 23:40:46 +0330

    insert members

--- data/sample_data.sql
+++ data/sample_data.sql
@@ -15,3 +15,8 @@ INSERT INTO books (title, author_id, category_id, year_published, total_copies, 
 ('مدیر مدرسه', 1, 1, 1958, 5, 5),
 ('قصه‌های مجید', 2, 2, 1990, 3, 3),
 ('غرور و تعصب', 3, 3, 1813, 4, 4);
+
+-- اعضا
+INSERT INTO members (name, email, join_date) VALUES 
+('علی رضایی', 'ali@email.com', '2024-01-10'),
+('سارا محمدی', 'sara@email.com', '2024-02-20');
