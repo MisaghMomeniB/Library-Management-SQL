b7c4a0d52dfafd86f6a166e26c6497db016b4f64
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-05-01 21:40:45 +0330

    create authors table

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -1,3 +1,9 @@
 -- Create Data Base
 CREATE DATABASE library_db;
 USE library_db;
+
+-- Authors Table
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY;
+    name VARCHAR(100) NOT NULL
+);
