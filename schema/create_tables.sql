07d091f1242dea5ac74456d9ccb2df2512e0e12d
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-05-01 21:44:43 +0330

    create book table

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -13,3 +13,16 @@ CREATE TABLE caregories (
     caregory_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+-- Book Table
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(225) NOT NULL,
+    author_id INT,
+    caregorie_id INT,
+    year_published INT,
+    total_copies INT DEFAULT 1,
+    available_copies INT DEFAULT 1,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
