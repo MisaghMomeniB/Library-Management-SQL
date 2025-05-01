9cda173611b58b004a964d7f680ff755f6e9fc27
Misagh Momeni Bashusqeh <166559069+MisaghMomeniB@users.noreply.github.com>
2025-05-01 21:42:22 +0330

    sort table

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -4,6 +4,12 @@ USE library_db;
 
 -- Authors Table
 CREATE TABLE authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY;
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+-- Sort Table
+CREATE TABLE caregories (
+    caregory_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
