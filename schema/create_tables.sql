01ad51df0e8223c29a793699bd03239f73f7a7b8
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-11 21:20:51 +0330

    authors table

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -1,3 +1,11 @@
 -- Create the database
 CREATE DATABASE IF NOT EXISTS library_db;
 USE library_db;
+
+-- Authors table
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    biography TEXT,
+    birth_date DATE
+);
