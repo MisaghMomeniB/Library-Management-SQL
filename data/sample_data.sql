773afbed006fb96731700a9c1308dfe870f784ca
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-14 16:29:09 +0330

    authors table

--- data/sample_data.sql
+++ data/sample_data.sql
@@ -1,2 +1,8 @@
 -- Use the library_db database
 USE library_db;
+
+-- AUTHORS table: inserting three authors
+INSERT INTO authors (name) VALUES
+('Jalal Al-e-Ahmad'),        -- Persian author of "The School Principal"
+('Houshang Moradi Kermani'), -- Known for children's books like "The Tales of Majid"
+('Jane Austen');             -- British classic novelist, author of "Pride and Prejudice"
