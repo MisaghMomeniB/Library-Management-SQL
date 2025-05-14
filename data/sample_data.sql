15096d94d423f911bb26d5ca308a9f12d2e4fa35
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-14 16:29:45 +0330

    categories table

--- data/sample_data.sql
+++ data/sample_data.sql
@@ -5,4 +5,10 @@ USE library_db;
 INSERT INTO authors (name) VALUES
 ('Jalal Al-e-Ahmad'),        -- Persian author of "The School Principal"
 ('Houshang Moradi Kermani'), -- Known for children's books like "The Tales of Majid"
-('Jane Austen');             -- British classic novelist, author of "Pride and Prejudice"
+('Jane Austen');
+
+-- CATEGORIES table: inserting three book categories
+INSERT INTO categories (name) VALUES
+('Novel'),             -- General novel category
+('Short Story'),       -- For short story books
+('Classic Literature');-- Works considered classics
